b3bbd07b89a1c7b94a0242d7549ea1b35a9f9643
Sridhr <sridharkonda553@gmail.com>
2026-04-12 11:20:24 +0530

    Fix blank screen on Render by making Vite base path conditional

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/Secure-banking-system/',
+  base: process.env.GITHUB_PAGES ? '/Secure-banking-system/' : '/',
   cacheDir: './node_modules/.vite_new_cache', // Bypassing locked folder
   server: {
     open: true,
